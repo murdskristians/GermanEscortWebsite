bac6d34e3c5e6a0635e6aa2dbae44a5928a9429d
Kristians <murdskristians@gmail.com>
2024-05-15 17:02:26 +0300

    Working on age confirmation page

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta https-equiv="X-UA-Compatible" wrapper="ie=edge">
+    <title>ESCORT</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,700&display=swap" rel="stylesheet">
+<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>-->
+<!--    <link rel="stylesheet" href="css/reset.css">-->
+<!--    <link rel="stylesheet" href="css/start.css?--><?php //echo microtime(); ?><!--">-->
+</head>
+
+<body>
+
+<?php // include_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php'; ?>
+
+
+
+
+<?php // include_once $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php'; ?>
+<!---->
+<!--<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>-->
+<!--<script src="/js/start_new.js"></script>-->
+</body>
+
+</html>
---
+++ pages/age_confirm_view.php
@@ -0,0 +1,86 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta https-equiv="X-UA-Compatible" wrapper="ie=edge">
+    <title>ESCORT</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,700&display=swap"
+          rel="stylesheet">
+    <!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>-->
+    <link rel="stylesheet" href="../css/reset.css?<?php echo microtime(); ?>">
+    <link rel="stylesheet" href="../css/vars.css?<?php echo microtime(); ?>">
+    <link rel="stylesheet" href="../css/style.css?<?php echo microtime(); ?>">
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="testimonial-20">
+        <img class="logogfu-4" src="../images/logogfu4.svg"/>
+        <div class="frame-173">
+            <div class="this-is-an-adult-website">This is an adult website</div>
+            <div
+                    class="this-website-contains-age-restricted-materials-including-nudity-and-explicit-depictions-of-sexual-activity-by-entering-you-affirm-that-you-are-at-least-18-years-of-age-or-the-age-of-majority-in-the-jurisdiction-you-are-accessing-the-website-from-and-you-consent-to-viewing-sexually-explicit-content"
+            >
+                This website contains age-restricted materials including nudity and
+                explicit depictions of sexual activity. By entering, you affirm that you
+                are at least 18 years of age or the age of majority in the jurisdiction
+                you are accessing the website from and you consent to viewing sexually
+                explicit content.
+            </div>
+        </div>
+        <div class="frame-174">
+            <div class="content-button">
+                <div class="buton">I am 18 or older</div>
+                <img class="arrow" src="../images/arrow.svg"/>
+            </div>
+            <div class="content-button">
+                <div class="buton">Enter I am under 18</div>
+                <img class="arrow" src="../images/arrow.svg"/>
+            </div>
+        </div>
+        <div
+                class="our-parental-controls-page-explains-how-you-can-easily-block-access-to-this-site"
+        >
+        <span>
+          <span
+                  class="our-parental-controls-page-explains-how-you-can-easily-block-access-to-this-site-span"
+          >
+            Our
+          </span>
+          <span
+                  class="our-parental-controls-page-explains-how-you-can-easily-block-access-to-this-site-span2"
+          >
+            parental controls page
+          </span>
+          <span
+                  class="our-parental-controls-page-explains-how-you-can-easily-block-access-to-this-site-span3"
+          >
+            explains how you can easily
+            <br/>
+            block access to this site.
+          </span>
+        </span>
+        </div>
+    </div>
+
+    <img id="red-dot-small" src="../images/red-dot-small.svg"/>
+    <img id="red-dot-big" src="../images/red-dot-big.svg"/>
+
+
+</div>
+
+<!---->
+<!--<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>-->
+<!--<script src="/js/start_new.js"></script>-->
+</body>
+
+</html>
