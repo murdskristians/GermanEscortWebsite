5f258cbd2425a6194fc39de4f797d65bc2325b52
Kristians <murdskristians@gmail.com>
2024-05-24 17:27:05 +0300

    made join us page

--- pages/faq_page.php
+++ pages/faq_page.php
@@ -30,120 +30,120 @@
     </div>
 
     <div class="faq_page-list">
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">1. How does a booking work?</div>
-                <img class="faq_page-frame-66" src="frame-660.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">2. How do I pay?</div>
-                <img class="faq_page-frame-662" src="frame-661.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">3. What is included in the price?</div>
-                <img class="faq_page-frame-663" src="frame-662.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">4. Can I talk to the call girl beforehand?</div>
-                <img class="faq_page-frame-664" src="frame-663.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">5. Can I express a dress request for the lady?</div>
-                <img class="faq_page-frame-665" src="frame-664.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">6. How are the escort girls dressed?</div>
-                <img class="faq_page-frame-666" src="frame-665.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">7. Can I tell the support team about my sex preferences?</div>
-                <img class="faq_page-frame-667" src="frame-666.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">8. Can I also visit the escort lady?</div>
-                <img class="faq_page-frame-668" src="frame-667.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">9. How quickly can an escort girl come to me?</div>
-                <img class="faq_page-frame-669" src="frame-668.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">10. Does the lady also have sex toys with her?</div>
-                <img class="faq_page-frame-6610" src="frame-669.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">11. Who can I contact if I have a complaint?</div>
-                <img class="faq_page-frame-6611" src="frame-6610.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
                 saņemšanas, pasūtījums tiks nodots tālākai izpildei.
             </div>
         </div>
-        <div class="faq_page-accordeon">
+        <div class="faq_page-accordion">
             <div class="faq_page-frame-60" onclick="toggleAnswer(this)">
                 <div class="faq_page-accord-on">12. Can I cancel a booking?</div>
-                <img class="faq_page-frame-6612" src="frame-6611.svg" />
+                <img class="faq_page-frame-faq-arrow" src="../images/icons/icon-faq-arrow.svg" alt="faq-arrow"/>
             </div>
             <div class="faq_page-frame-63 faq_page-answer">
                 Nē. Lai saņemtu pasūtījumu, ir jāveic priekšapmaksa un pēc maksājuma
@@ -155,7 +155,7 @@
 </div>
 <script>
     document.addEventListener("DOMContentLoaded", function () {
-        const accordions = document.querySelectorAll(".faq_page-accordeon");
+        const accordions = document.querySelectorAll(".faq_page-accordion");
 
         accordions.forEach(accordion => {
             const question = accordion.querySelector(".faq_page-frame-60");
---
+++ pages/join_the_team_page.php
@@ -0,0 +1,120 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta https-equiv="X-UA-Compatible" wrapper="ie=edge">
+    <title>Join the team</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,700&display=swap"
+          rel="stylesheet">
+    <!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>-->
+    <link rel="stylesheet" href="../css/reset.css?<?php echo microtime(); ?>">
+    <link rel="stylesheet" href="../css/vars.css?<?php echo microtime(); ?>">
+    <link rel="stylesheet" href="../css/style.css?<?php echo microtime(); ?>">
+</head>
+
+<body>
+<?php  include_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php'; ?>
+
+
+<div class="common-main-section">
+
+
+    <section class="join_us_page-container">
+        <div class="join_us_page-wrapper">
+            <h1 class="join_us_page-title">Join the team</h1>
+            <div class="join_us_page-section-content">
+                <div class="join_us_page-columns">
+                    <article class="join_us_page-column">
+                        <div class="join_us_page-column-content">
+                            <img loading="lazy" src="../images/join_the_team_female.png" class="join_us_page-image" alt="Independet Escort" />
+                            <h2 class="join_us_page-heading">Independet Escort</h2>
+                            <p class="join_us_page-paragraph"> Please read these terms and conditions carefully before using this website. Accepting these conditions signifies that you have read </p>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Free sign up</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Manage your profile</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Promote yourself</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Access to blacklist</span>
+                                </li>
+                            </ul>
+                            <div tabindex="0" role="button" class="join_us_page-action-button">
+                                <span class="join_us_page-action-text">Sign up</span>
+                                <img loading="lazy" src="../images/icons/icon-join_us_page-arrow.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                            </div>
+                        </div>
+                    </article>
+                    <article class="join_us_page-column-2">
+                        <div class="join_us_page-column-2-content">
+                            <img loading="lazy" src="../images/join_the_team_male.png" class="join_us_page-image" alt="Member" />
+                            <h2 class="join_us_page-heading">Member</h2>
+                            <p class="join_us_page-paragraph"> Please read these terms and conditions carefully before using this website. Accepting these conditions signifies that you have read, </p>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Free sign up</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Add reviews</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Manage favorites</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>Access to blacklist</span>
+                                </li>
+                            </ul>
+                            <ul class="join_us_page-feature-list">
+                                <li class="join_us_page-feature-item">
+                                    <img loading="lazy" src="../images/icons/icon-join_the_team-red-check.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                                    <span>New girls in your city</span>
+                                </li>
+                            </ul>
+                            <div tabindex="0" role="button" class="join_us_page-action-button-2">
+                                <span class="join_us_page-action-text">Sign up</span>
+                                <img loading="lazy" src="../images/icons/icon-join_us_page-arrow.svg" class="join_us_page-icon" alt="Join us page checkmark" />
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+<?php  include_once $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php'; ?>
+
+</body>
+</html>
